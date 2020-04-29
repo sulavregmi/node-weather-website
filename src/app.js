43b5a7d468c1a59017d1a23f5a0f61633770e373
Sulav <sulav.regmi44@gmail.com>
2020-04-29 16:13:32 +0545

    setup app for heroku:

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utilis/forecast')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express Config
 const publicDirectoryPath = (path.join(__dirname, '../public'))
@@ -120,7 +121,7 @@ app.get('*',(req,res) =>{
         errorMessage : 'Page not found'
     })
 })
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
 
 })
